9afc15fb7c918534d2d45af6d5ed498ef1b7e883
Yusuke Kuoka <ykuoka@gmail.com>
2024-01-28 12:50:32 +0000

    Add Kubectl.Failed helper for running failable commands without stopping the test

--- kubectl.go
+++ kubectl.go
@@ -12,12 +12,31 @@ import (
 type Kubectl struct {
 	// KubeconfigPath is the path to the kubeconfig file.
 	KubeconfigPath string
+
+	// LogError controls whether to log the error returned by kubectl.
+	// Currently, this is respected that doesn't return the error,
+	// like the `Failed` method.
+	LogError bool
 }
 
 func NewKubectl(kubeconfigPath string) *Kubectl {
 	return &Kubectl{
 		KubeconfigPath: kubeconfigPath,
 	}
+}
+
+// Failed runs kubectl with the given args and returns true if it fails.
+// This is useful for e.g. asserting that a resource does not exist.
+func (k *Kubectl) Failed(t *testing.T, args ...string) bool {
+	t.Helper()
+
+	_, err := k.capture(args...)
+
+	if k.LogError {
+		t.Log(err)
+	}
+
+	return err != nil
 }
 
 func (k *Kubectl) Capture(t *testing.T, args ...string) string {
